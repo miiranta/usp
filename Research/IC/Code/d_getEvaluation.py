f2a41ddca19647d589da989a63dcd4cedf874fbf
Lucas Miranda <lucasrezende12@hotmail.com>
2025-09-22 17:54:02 -0300

    IC - fix

--- Research/IC/Code/d_getEvaluation.py
+++ Research/IC/Code/d_getEvaluation.py
@@ -141,7 +141,7 @@ class Evaluation:
             self.grade = -2
             return
         try:
-            inputs = PROMPT + self.sentence
+            inputs = PROMPT + self.sentence + "\nRESPOSTA:"
             inputs = loaded_tokenizer(inputs, return_tensors="pt")
             with torch.no_grad():
                 generated = loaded_model.generate(**inputs, max_new_tokens=2, do_sample=False, pad_token_id=loaded_tokenizer.eos_token_id)
