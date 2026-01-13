9e1e91a6e25faa62f10ec7170191a30b1b1ba0e5
Lucas Miranda <lucas.miranda@oreons.com>
2026-01-12 22:40:22 -0300

    nature - test 1.3

--- Research/Complexity_Training_Test_2/train_nature.py
+++ Research/Complexity_Training_Test_2/train_nature.py
@@ -49,7 +49,7 @@ class Config:
     MAX_SAMPLES = 100
     
     # Number of runs
-    NUM_OF_RUN_PER_CALL = 3
+    NUM_OF_RUN_PER_CALL = 1
     
     # Complexity calculation interval
     COMPLEXITY_UPDATE_INTERVAL = 1 
